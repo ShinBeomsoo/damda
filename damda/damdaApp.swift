9679d69aa99e38b1792e98b07f3d8db33a4d4826
ShinBeomsoo <beomsooshin.leo@gmail.com>
2025-08-06 20:59:11 +0900

    Initial Commit

---
+++ damda/damdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  damdaApp.swift
+//  damda
+//
+//  Created by SHIN BEOMSOO on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct damdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ damdaTests/damdaTests.swift
@@ -0,0 +1,17 @@
+//
+//  damdaTests.swift
+//  damdaTests
+//
+//  Created by SHIN BEOMSOO on 8/6/25.
+//
+
+import Testing
+@testable import damda
+
+struct damdaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
